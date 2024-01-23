636852c0a952dc4b6f57d9d69c7e4ebb726dfd28
rajesh-nyk <157436640+rajesh-nyk@users.noreply.github.com>
2024-01-23 08:14:46 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
+let v=0;
 if (/Mobi | Andriod/i.test(navigator.userAgent)) {
     alert("User is on mobile");
+    v=1;
 }
 else {
     console.log("User is on desktop");
+    v=2;
+}
+console.log(v);
+let s=document.getElementById('name');
+console.log(s);
+if(v==2) {
+    s.textContent = 'Hi Desktop User';
+}
+else if(v==1) {
+    s.textContent = 'Hi Desktop User';
+}
+else {
+
 }
